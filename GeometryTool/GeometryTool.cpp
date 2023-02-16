8d764e703dde0d6da2bade7e40e9aac6234418fa
nikolay5r <nradilov30@gmail.com>
2023-02-17 00:56:18 +0200

    Move all print functions to "Print.h" header

--- GeometryTool/GeometryTool.cpp
+++ GeometryTool/GeometryTool.cpp
@@ -11,6 +11,7 @@
 #include "Animation.h"
 #include "Helper.h"
 #include "Validation.h"
+#include "Print.h"
 
 const int timeToWait = 1200;
 const int maxLengthOfName = 16;
@@ -38,50 +39,6 @@ const char* NAME_DOESNT_EXIST_TEXT = "Name doesn't exist! Try another. . .\n";
 const char* INVALID_NUMBER_TEXT = "Number is too large! It should be between -100 and 100!\n";
 
 bool stopProgram = false;
-
-void printLine(const double k, double n, const double x1, const double y1, const double x2 = 102, const double y2 = 102)
-{
-	std::string symbol = n >= 0 ? "+" : "-";
-	n = abs(n);
-	std::cout << "The equation of the line is: ";
-
-	if (x1 == x2)
-	{
-		std::cout << "x = " << x1;
-	}
-	else if (y1 == y2)
-	{
-		std::cout << "y = " << y1;
-	}
-	else if (k != 1 && k != 0)
-	{
-		if (n == 0)
-		{
-			std::cout << "y = " << k << " * x";
-		}
-		else
-		{
-			std::cout << "y = " << k << " * x " << symbol << " " << n;
-		}
-	}
-	else if (k == 1)
-	{
-		std::cout << "y = x " << symbol << " " << n;
-	}
-	else
-	{
-		if (symbol == "+")
-		{
-			std::cout << "y = " << n;
-		}
-		else
-		{
-			std::cout << "y = " << symbol << n;
-		}
-	}
-
-	std::cout << "\n";
-}
 
 void getN(double& n)
 {
@@ -142,26 +99,6 @@ std::string getKeywordFromConsole()
 	convertToLowerCase(keyword);
 
 	return keyword;
-}
-
-void printTitle()
-{
-	std::cout << "\n\t\t\t---GeometryTool---\n";
-}
-
-void printMainMenu()
-{
-	std::cout << "\nEnter a number corresponding to the option you want to choose or enter \"exit\" if you want to exit:\n";
-
-	std::cout << " " << SAVE_OR_DELETE_CORRESPONDING_NUMBER << " - save a line or a point or delete one;\n"
-		<< " " << DEFINE_LINE_CORRESPONDING_NUMBER << " - define a line through slope and points;\n"
-		<< " " << CHECK_IF_POINT_IS_ON_LINE_CORRESPONDING_NUMBER << " - check if a point is on a line;\n"
-		<< " " << FIND_PARALEL_LINE_CORRESPONDING_NUMBER << " - derive an equation of a line that is parallel to a given line and passes through a point;\n"
-		<< " " << FIND_PERPENDICULAR_LINE_CORRESPONDING_NUMBER << " - derive an equation of a perpendicular line to a given line and passes through a point;\n"
-		<< " " << FIND_INTERSECTION_POINT_CORRESPONDING_NUMBER << " - find intersection points (if it exists);\n"
-		<< " " << FIND_EQUATIONS_IN_TRIANGLE_CORRESPONDING_NUMBER << " - find equations in triangle;\n"
-		<< " " << FIND_THE_TANGENT_CORRESPONDING_NUMBER << " - derive an equation of a tangent to a parabola at a corresponding point;\n"
-		<< " " << DETERMINE_THE_TYPE_OF_TETRAGON_CORRESPONDING_NUMBER << " - given four equations to determine the type of tetragon.\n";
 }
 
 void getEquationOfLine(const std::string name, std::string& equation)
